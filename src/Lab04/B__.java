f8a82ba3262c0627e73245b1ce12caba3cf533f7
Hibrul Anam Prantik <hibrul.anam.prantik@g.bracu.ac.bd>
2025-08-05 21:59:14 +0600

    B. Adjacency List Representation -> Updated | Fixed some minor issues

--- src/Lab04/B__.java
+++ src/Lab04/B__.java
@@ -1,45 +1,45 @@
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class B__ {
     public static void main(String[] args) throws Exception {
         java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
-        String [] line1 = br.readLine().split(" ");
+        String[] line1 = br.readLine().split(" ");
         int N = Integer.parseInt(line1[0]), M = Integer.parseInt(line1[1]);
 
         int[] u = new int[M], v = new int[M], w = new int[M];
 
-        String uElem[] = br.readLine().split(" ");
-        String vElem[] = br.readLine().split(" ");
-        String wElem[] = br.readLine().split(" ");
-        for(int i = 0; i < M; ++i) {
+        String uElem[] = br.readLine().trim().split("\\s+");
+        String vElem[] = br.readLine().trim().split("\\s+");
+        String wElem[] = br.readLine().trim().split("\\s+");
+        for (int i = 0; i < M; ++i) {
             u[i] = Integer.parseInt(uElem[i]);
             v[i] = Integer.parseInt(vElem[i]);
             w[i] = Integer.parseInt(wElem[i]);
         }
         // add to the graph
-        LinkedList<Edge>[]adjList = new LinkedList[N + 1];  // array of linked lists -> contains edges
+        List<List<Edge>> adjList = new ArrayList<>(N + 1); // List of lists -> contains edges
         // Initializing each list
-        for(int i = 1; i <= N; ++i) {
-            adjList[i] = new LinkedList<>(); 
-        }  
+        for (int i = 0; i <= N; ++i) adjList.add(new ArrayList<>());  // indices 0,...,N
+        
         // adding edges to the graph
-        for(int i = 0; i < M; ++i) {
-            adjList[u[i]].add(new Edge(v[i], w[i])); 
-        }   
+        for (int i = 0; i < M; ++i) adjList.get(u[i]).add(new Edge(v[i], w[i]));
         
         // print the graph
-        for(int i = 1; i <= N; ++i) {
+        for (int i = 1; i <= N; i++) {
             System.out.print(i + ":");
-            for(Edge edge : adjList[i]) {
-                System.out.print("(" + edge.vertex + "," + edge.weight + ") ");
+            for (Edge e : adjList.get(i)) {
+                System.out.print("(" + e.vertex + "," + e.weight + ") ");
             }
             System.out.println();
         }
+        br.close();
     }
 
     // Helper class to represent a pair/Edges (vertex, weight)
     static class Edge {
         int vertex, weight;
+
         Edge(int v, int w) {
             vertex = v;
             weight = w;
